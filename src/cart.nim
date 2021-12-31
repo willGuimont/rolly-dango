4296c5f57ff2b9661dd20b7ab2e2c3d8ddfbd6a1
samX500 <samX500@outlook.com>
2021-12-30 21:03:21 -0500

    some tiles rendered

--- src/cart.nim
+++ src/cart.nim
@@ -11,35 +11,60 @@ proc NimMain {.importc.}
 proc start {.exportWasm.} =
   NimMain()
 
+var decal: tuple[x: int32, y: int32, z: int32] = (x: int32(7), y: int32(4),
+    z: int32(6))
+var origin: tuple[x: int32, y: int32] = (x: int32(80), y: int32(80))
+
+proc position_to_iso(position: PositionComponent): tuple[x: int32, y: int32] =
+  var iso_x: int32 = int32(position.x) * -decal.x + int32(position.y)*decal.x +
+      int32(position.z)*0 + origin.x
+  var iso_y: int32 = int32(position.x) * decal.y + int32(position.y) * decal.y +
+      int32(position.z) * -decal.z + origin.y
+  return (x: iso_x, y: isoY)
+
 proc render(reg: Registry) {.exportWasm.} =
+  DRAW_COLORS[] = 0x0320
+
   for entity in entitiesWith(reg, SpriteComponent):
-    var component = getComponent[SpriteComponent](reg, entity)
-    blit(addr component.sprite.data[0], 76, 76, component.sprite.width,
-        component.sprite.height, component.sprite.flags)
+    let (spriteComponent, positionComponent) = reg.getComponents(entity,
+        SpriteComponent, PositionComponent)
+    let position = position_to_iso(positionComponent)
+    blit(addr spriteComponent.sprite.data[0], position.x, position.y,
+        spriteComponent.sprite.width,
+
+spriteComponent.sprite.height, spriteComponent.sprite.flags)
 
 
 proc update {.exportWasm.} =
   var reg = newRegistry()
-  var tileEntity = reg.newEntity()
-  var tileSpriteComponent = SpriteComponent(sprite: tile16px)
-  var tilePositionComponent: PositionComponent = PositionComponent(x: 0, y: 0, z: 0)
-  reg.addComponent(tileEntity, tileSpriteComponent)
-  #reg.addComponent(tileEntity, tilePositionComponent)
+  var tileEntity1 = reg.newEntity()
+  var tileSpriteComponent1 = SpriteComponent(sprite: tile16px)
+  var tilePositionComponent1: PositionComponent = PositionComponent(x: 0, y: 0, z: 0)
+  reg.addComponent(tileEntity1, tileSpriteComponent1)
+  reg.addComponent(tileEntity1, tilePositionComponent1)
+
+  var tileEntity2 = reg.newEntity()
+  var tileSpriteComponent2 = SpriteComponent(sprite: tile16px)
+  var tilePositionComponent2: PositionComponent = PositionComponent(x: 1, y: 0, z: 0)
+  reg.addComponent(tileEntity2, tileSpriteComponent2)
+  reg.addComponent(tileEntity2, tilePositionComponent2)
+
+  var tileEntity3 = reg.newEntity()
+  var tileSpriteComponent3 = SpriteComponent(sprite: tile16px)
+  var tilePositionComponent3: PositionComponent = PositionComponent(x: 0, y: 1, z: 0)
+  reg.addComponent(tileEntity3, tileSpriteComponent3)
+  reg.addComponent(tileEntity3, tilePositionComponent3)
+
+  var tileEntity4 = reg.newEntity()
+  var tileSpriteComponent4 = SpriteComponent(sprite: tile16px)
+  var tilePositionComponent4: PositionComponent = PositionComponent(x: 1, y: 1, z: 0)
+  reg.addComponent(tileEntity4, tileSpriteComponent4)
+  reg.addComponent(tileEntity4, tilePositionComponent4)
 
   var dangoEntity = reg.newEntity()
   var dangoSpriteComponent = SpriteComponent(sprite: dango16px)
   var dangoPositionComponent: PositionComponent = PositionComponent(x: 0, y: 0, z: 1)
   reg.addComponent(dangoEntity, dangoSpriteComponent)
-  #reg.addComponent(dangoEntity, dangoPositionComponent)
+  reg.addComponent(dangoEntity, dangoPositionComponent)
 
   render(reg)
-
-  DRAW_COLORS[] = 2
-  text("Hello from Nim!", 10, 10)
-
-  var gamepad = GAMEPAD1[]
-  if bool(gamepad and BUTTON_1):
-    DRAW_COLORS[] = 4
-
-  blit(addr dango16px.data[0], 76, 76, 8, 8, BLIT_1BPP)
-  text("Press X to blink", 16, 90)
