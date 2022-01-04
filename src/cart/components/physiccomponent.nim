3b66e3409a897a297d3c5f7095afc9d0028cbb51
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2022-01-03 21:58:43 -0500

    fixed comments

--- src/cart/components/physiccomponent.nim
+++ src/cart/components/physiccomponent.nim
@@ -1,3 +1,4 @@
+import std/options
 import ../ecs/ecs
 import ../components/positioncomponent
 import ../components/worldtilecomponent
@@ -5,14 +6,16 @@ import ../components/worldtilecomponent
 type
     PhysicsComponent* = ref object of Component
 
+proc standingOn(reg: Registry, pos: PositionComponent): Option[Entity] =
+    for e in reg.entitiesWith(PositionComponent, WorldTileComponent):
+        let tpos = reg.getComponent[:PositionComponent](e)
+        if tpos.x == pos.x and tpos.y == pos.y and tpos.z == pos.z - 1:
+            return some(e)
+    return none(Entity)
+
 proc physicsSystem*(reg: Registry) =
     for (pos, _) in reg.entitiesWithComponents(PositionComponent,
             PhysicsComponent):
-        var floorUnder = false
-        for (tpos, _) in reg.entitiesWithComponents(PositionComponent,
-                WorldTileComponent):
-            if tpos.x == pos.x and tpos.y == pos.y and tpos.z == pos.z - 1:
-                floorUnder = true
-                break
-        if not floorUnder:
+        let entityUnder = reg.standingOn(pos)
+        if entityUnder.isNone():
             pos.z.dec
