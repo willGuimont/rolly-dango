da5be9b35c734b5d6ac16f51881c2b68145ff8b8
samX500 <samX500@outlook.com>
2022-01-04 19:01:46 -0500

    WIP, adding slope physics

--- src/cart/components/physiccomponent.nim
+++ src/cart/components/physiccomponent.nim
@@ -4,7 +4,46 @@ import ../components/positioncomponent
 import ../components/worldtilecomponent
 
 type
+    Direction* = enum
+        dNone, dRight, dFront, dLeft, dBack
     PhysicsComponent* = ref object of Component
+        velocity*: uint8
+        direction*: Direction
+
+proc getSlopeDirection(worldTile: WorldTileType): Direction =
+    if worldTile == WorldTileType.wttSlopeRight:
+        return Direction.dRight
+    elif worldTile == WorldTileType.wttSlopeFront:
+        return Direction.dFront
+    elif worldTile == WorldTileType.wttSlopeLeft:
+        return Direction.dLeft
+    elif worldTile == WorldTileType.wttSlopeBack:
+        return Direction.dBack
+    else:
+        return Direction.dNone
+
+proc isOpposite(direction: Direction, otherDirection: Direction): bool =
+    if direction == dNone:
+        return false
+    elif direction == dRight:
+        return otherDirection == dLeft
+    elif direction == dFront:
+        return otherDirection == dBack
+    elif direction == dLeft:
+        return otherDirection == dRight
+    elif direction == dBack:
+        return otherDirection == dFront
+
+proc changeVelocityOnSlope(phy: PhysicsComponent, slopeDirection: Direction) =
+    if phy.velocity == 0:
+        phy.velocity.inc()
+        phy.direction = slopeDirection
+    elif phy.direction == slopeDirection:
+        phy.velocity.inc()
+    elif phy.direction.isOpposite(slopeDirection):
+        phy.velocity.dec()
+        if phy.velocity == 0:
+            phy.direction = Direction.dNone
 
 proc standingOn(reg: Registry, pos: PositionComponent): Option[Entity] =
     for e in reg.entitiesWith(PositionComponent, WorldTileComponent):
@@ -13,9 +52,53 @@ proc standingOn(reg: Registry, pos: PositionComponent): Option[Entity] =
             return some(e)
     return none(Entity)
 
+proc moveDirection(pos: PositionComponent, direction: Direction) =
+    if direction == dNone:
+        return
+    elif direction == dRight:
+        pos.y.inc()
+    elif direction == dFront:
+        pos.x.inc()
+    elif direction == dLeft:
+        pos.y.dec()
+    elif direction == dBack:
+        pos.x.dec()
+
+proc moveFromVelocity(reg: Registry, pos: PositionComponent,
+        phy: PhysicsComponent) =
+    if phy.velocity == 0:
+        return
+    let entityUnder = reg.standingOn(pos)
+    if entityUnder.isNone:
+        return
+
+    let entity: Entity = entityUnder.get()
+    let tileUnder: WorldTileType = reg.getComponents(entity,
+                    WorldTileComponent)[0].worldTile
+    if tileUnder == wttTile:
+        moveDirection(pos, phy.direction)
+    if tileUnder == wttSlopeRight:
+        let slopeDirection: Direction = getSlopeDirection(wttSlopeRight)
+        if phy.direction == slopeDirection:
+            pos.y.inc()
+            pos.z.dec()
+
 proc physicsSystem*(reg: Registry) =
-    for (pos, _) in reg.entitiesWithComponents(PositionComponent,
+    for (pos, phy) in reg.entitiesWithComponents(PositionComponent,
             PhysicsComponent):
+        moveFromVelocity(reg, pos, phy)
         let entityUnder = reg.standingOn(pos)
         if entityUnder.isNone():
             pos.z.dec
+        else:
+            let entity: Entity = entityUnder.get()
+            let tileUnder: WorldTileType = reg.getComponents(entity,
+                    WorldTileComponent)[0].worldTile
+            if tileUnder == wttTile:
+                if phy.velocity > 0:
+                    phy.velocity.dec()
+                    if phy.velocity == 0:
+                        phy.direction = Direction.dNone
+            elif tileUnder == WorldTileType.wttSlopeRight:
+                changeVelocityOnSlope(phy, dRight)
+                moveFromVelocity(reg, pos, phy)
--- tests/test_components_physiccomponent.nim
+++ tests/test_components_physiccomponent.nim
@@ -33,3 +33,100 @@ suite "physiccomponent":
         let pos = reg.getComponent[:PositionComponent](entity)
         check pos.z == 2
 
+    test "When entity has velocity on flat tile, moves forward and looses velocity":
+        var reg = newRegistry()
+        let entity = reg.newEntity()
+        reg.addComponent(entity, PositionComponent(x: 0, y: 0, z: 1))
+        reg.addComponent(entity, PhysicsComponent())
+        let tile1 = reg.newEntity()
+        reg.addComponent(tile1, PositionComponent(x: 0, y: 0, z: 0))
+        reg.addComponent(tile1, WorldTileComponent(worldTile: wttTile))
+        let tile2 = reg.newEntity()
+        reg.addComponent(tile2, PositionComponent(x: 0, y: 1, z: 0))
+        reg.addComponent(tile2, WorldTileComponent(worldTile: wttTile))
+        let phy = reg.getComponent[:PhysicsComponent](entity)
+
+        phy.velocity = 1
+        phy.direction = dRight
+
+        reg.physicsSystem()
+        #This call should have no impact, if it does it's a problem
+        reg.physicsSystem()
+
+        let pos = reg.getComponent[:PositionComponent](entity)
+
+        check pos.y == 1
+        check pos.z == 1
+        check phy.velocity == 0
+        check phy.direction == dNone
+
+    test "When on right slope, should gain velocity in slope direction":
+        var reg = newRegistry()
+        let entity = reg.newEntity()
+        reg.addComponent(entity, PositionComponent(x: 0, y: 0, z: 2))
+        reg.addComponent(entity, PhysicsComponent())
+        let slopeRight = reg.newEntity()
+        reg.addComponent(slopeRight, PositionComponent(x: 0, y: 0, z: 1))
+        reg.addComponent(slopeRight, WorldTileComponent(
+                worldTile: wttSlopeRight))
+
+        reg.physicsSystem()
+
+        let pos = reg.getComponent[:PositionComponent](entity)
+        let phy = reg.getComponent[:PhysicsComponent](entity)
+        check pos.y == 0
+        check pos.z == 2
+        check phy.velocity == 1
+        check phy.direction == Direction.dRight
+
+    test "When on right slope, move to right and down, increase velocity":
+        var reg = newRegistry()
+        let entity = reg.newEntity()
+        reg.addComponent(entity, PositionComponent(x: 0, y: 0, z: 2))
+        reg.addComponent(entity, PhysicsComponent())
+        let slopeRight = reg.newEntity()
+        reg.addComponent(slopeRight, PositionComponent(x: 0, y: 0, z: 1))
+        reg.addComponent(slopeRight, WorldTileComponent(
+                worldTile: wttSlopeRight))
+        let tile1 = reg.newEntity()
+        reg.addComponent(tile1, PositionComponent(x: 0, y: 1, z: 0))
+        reg.addComponent(tile1, WorldTileComponent(worldTile: wttTile))
+
+        reg.physicsSystem()
+        reg.physicsSystem()
+
+        let pos = reg.getComponent[:PositionComponent](entity)
+        let phy = reg.getComponent[:PhysicsComponent](entity)
+        check pos.y == 1
+        check pos.z == 1
+        check phy.velocity == 1
+        check phy.direction == Direction.dRight
+
+    test "When sliding from slope to tile, preserve momentum":
+        var reg = newRegistry()
+        let entity = reg.newEntity()
+        reg.addComponent(entity, PositionComponent(x: 0, y: 0, z: 2))
+        reg.addComponent(entity, PhysicsComponent())
+        let slopeRight = reg.newEntity()
+        reg.addComponent(slopeRight, PositionComponent(x: 0, y: 0, z: 1))
+        reg.addComponent(slopeRight, WorldTileComponent(
+                worldTile: wttSlopeRight))
+        let tile1 = reg.newEntity()
+        reg.addComponent(tile1, PositionComponent(x: 0, y: 1, z: 0))
+        reg.addComponent(tile1, WorldTileComponent(worldTile: wttTile))
+        let tile2 = reg.newEntity()
+        reg.addComponent(tile2, PositionComponent(x: 0, y: 2, z: 0))
+        reg.addComponent(tile2, WorldTileComponent(worldTile: wttTile))
+
+        reg.physicsSystem()
+        reg.physicsSystem()
+        reg.physicsSystem()
+        #This call should have no impact, if it does it's a problem
+        reg.physicsSystem()
+
+        let pos = reg.getComponent[:PositionComponent](entity)
+        let phy = reg.getComponent[:PhysicsComponent](entity)
+        check pos.y == 2
+        check pos.z == 1
+        check phy.velocity == 0
+        check phy.direction == Direction.dNone
