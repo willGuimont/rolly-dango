9e33917c8c4b1ae50dd0575094f1bcae1e82f236
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2022-01-11 21:48:25 -0500

    animation creation

--- src/cart/components/animationcomponent.nim
+++ src/cart/components/animationcomponent.nim
@@ -6,12 +6,13 @@ type
     AnimationFrame* = ref object
         frame*: Sprite
         shownFrame*: int
-    Animation* = seq[AnimationFrame]
+    Animation* = object
+        frames*: seq[AnimationFrame]
     AnimationComponent* = ref object of Component
 
-macro makeAnimationFrame(name: untyped): untyped =
-    let spriteIdent = ident($name & "AnimFrame")
+macro makeAnimationFrame*(name: untyped, shownFrame: int): untyped =
+    let spriteIdent = ident($name & "Sprite")
     let animIdent = ident($name & "AnimFrame")
     return quote do:
-
-
+        let `animIdent` = AnimationFrame(frame: `spriteIdent`,
+                shownFrame: `shownFrame`)
