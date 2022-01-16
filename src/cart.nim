5a44afe4f23270607df5d23d442ef73c514f0719
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2022-01-15 22:14:48 -0500

    level 06 wip

--- src/cart.nim
+++ src/cart.nim
@@ -13,6 +13,7 @@ import cart/assets/levels/rlevel01
 import cart/assets/levels/rlevel02
 import cart/assets/levels/rlevel03
 import cart/assets/levels/rlevel04
+import cart/assets/levels/rlevel06
 import cart/input/gamepad
 import cart/state/gamestatemachine
 import cart/assets/sprites
@@ -79,8 +80,7 @@ proc render(reg: Registry) =
 
 proc buildWorld() =
   reg = newRegistry()
-  let level = newLevelList(addr reg, addr theGamepad, @[unsafeAddr level04,
-      unsafeAddr level01, unsafeAddr level02, unsafeAddr level03])
+  let level = newLevelList(addr reg, addr theGamepad, @[unsafeAddr level06])
   sm = newStateMachine(level)
 
 proc setPalette() =
